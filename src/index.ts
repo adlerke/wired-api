8180cbf37244a9aa3dcc9bf62288de379a4ca78b
Adler Kesley <adlerbassk@gmail.com>
2022-11-14 23:04:05 -0300

    Create Models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-const port = 3001;
-app.listen(port, () => {
-    console.log(`🔥 Server running in ${port} port`);
-});
+
+mongoose
+    .connect("mongodb://localhost:27017")
+    .then(() => {
+        const port = 3001;
+        app.listen(port, () => {
+            console.log(`🔥 Server running in ${port} port`);
+        });
+    })
+    .catch((e) => {
+        console.log(e, "Erro de conexao");
+    });
